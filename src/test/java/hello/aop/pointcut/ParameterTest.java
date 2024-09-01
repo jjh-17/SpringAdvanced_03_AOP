04602c468d17bfe3f66f4f53b04f04e9004ef498
jjh <jjho311@naver.com>
2024-09-01 10:30:51 +0900

    Test: 포인트컷을 이용한 어드바이스 매개변수 전달

---
+++ src/test/java/hello/aop/pointcut/ParameterTest.java
@@ -0,0 +1,90 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(ParameterTest.ParameterAspect.class)
+@SpringBootTest
+public class ParameterTest {
+
+    @Autowired MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    // 포인트컷 정의 클래스
+    @Slf4j
+    @Aspect
+    static class ParameterAspect {
+        // member 내 모든 메서드
+        @Pointcut("execution(* hello.aop.member..*.*(..))")
+        private void allMember() {}
+
+        // member 내 모든 매서드
+        @Around("allMember()")
+        public Object logArgs1(ProceedingJoinPoint joinPoint) throws Throwable {
+            Object arg1 = joinPoint.getArgs()[0];
+            log.info("[logArgs1]{}, arg={}", joinPoint.getSignature(), arg1);
+            return joinPoint.proceed();
+        }
+
+        // arg로 시작으로 하는 매개변수를 가진 메서드 
+        @Around("allMember() && args(arg,..)")
+        public Object logArgs2(ProceedingJoinPoint joinPoint, Object arg) throws Throwable {
+            log.info("[logArgs2]{}, arg={}", joinPoint.getSignature(), arg);
+            return joinPoint.proceed();
+        }
+
+        // @Before를 이용하여 logArgs2 축약 및 Type String으로 제한
+        @Before("allMember() && args(arg,..)")
+        public void logArgs3(String arg) {
+            log.info("[logArgs3] arg={}", arg);
+        }
+
+        // obj 타입의 프록시 객체를 전달 받는 메서드
+        @Before("allMember() && this(obj)")
+        public void thisArgs(JoinPoint joinPoint, MemberService obj) {
+            log.info("[this]{}, obj={}", joinPoint.getSignature(), obj.getClass());
+        }
+
+        // obj 실제 객체를 전달 받는 경우
+        @Before("allMember() && target(obj)")
+        public void targetArgs(JoinPoint joinPoint, MemberService obj) {
+            log.info("[target]{}, obj={}", joinPoint.getSignature(), obj.getClass());
+        }
+
+        // annotation 타입의 애노테이션을 전달받는 자식 및 부모 클래스 모두에 적용
+        @Before("allMember() && @target(annotation)")
+        public void atTarget(JoinPoint joinPoint, ClassAop annotation) {
+            log.info("[@target]{}, obj={}", joinPoint.getSignature(), annotation);
+        }
+
+        // annotation 타입의 애노테이션을 전달받는 자식 클래스에만 적용
+        @Before("allMember() && @within(annotation)")
+        public void atWithin(JoinPoint joinPoint, ClassAop annotation) {
+            log.info("[@within]{}, obj={}", joinPoint.getSignature(), annotation);
+        }
+
+        // annotation 타입의 애노테이션을 가지는 메서드에 작용
+        @Before("allMember() && @annotation(annotation)")
+        public void atAnnotation(JoinPoint joinPoint, MethodAop annotation) {
+            log.info("[@annotation]{}, annotationValue={}", joinPoint.getSignature(), annotation.value());
+        }
+    }
+}
